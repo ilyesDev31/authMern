45ad62262035d409c469355d24a10bc143665e4e
ilyesDev31 <ibendjellal8@gmail.com>
2025-09-10 20:44:22 +0100

    first commit

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(hpp());
 app.use(helmet());
 app.use(
   cors({
-    origin: "https://authmern-1-3tbn.onrender.com/",
+    origin: "https://authmern-static.onrender.com/",
     credentials: true, // This allows cookies to be sent
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
