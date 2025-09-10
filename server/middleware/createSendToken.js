dbd7a843645173b307b9f74d7286ec08c30109bc
ilyesDev31 <ibendjellal8@gmail.com>
2025-09-10 20:57:21 +0100

    first commit

--- server/middleware/createSendToken.js
+++ server/middleware/createSendToken.js
@@ -3,7 +3,7 @@ module.exports = (user, statusCode, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true in production
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
     path: "/",
   };
