63cc1e88eb161f9359cfa0713223a199e7503d37
ilyesDev31 <ibendjellal8@gmail.com>
2025-09-13 11:10:21 +0100

    first commit

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ const path = require("path");
 
 app.use(express.json({ limit: "32kb" }));
 app.use(express.urlencoded({ extended: true, limit: "32kb" }));
-app.use(cookieParser());
 app.use(hpp());
 app.use(helmet());
 app.use(
@@ -20,6 +19,8 @@ app.use(
     credentials: true, // This allows cookies to be sent
   })
 );
+app.use(cookieParser());
+
 const limiting = limit({
   max: 10,
   windowMs: 10 * 60 * 1000,
--- server/middleware/createSendToken.js
+++ server/middleware/createSendToken.js
@@ -3,8 +3,8 @@ module.exports = (user, statusCode, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true in production
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    path: "/",
   };
 
   res.cookie("jwt", token, options).status(statusCode).json({
